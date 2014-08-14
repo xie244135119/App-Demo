cb6d47027e5440c3da70f42dfb222bfb999402f5
SunSet <SunSet@xieqiangdeMacBook-Pro.local>
2014-08-14 09:52:55 +0800

    Initial Commit

---
+++ Test淘宝首页/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Test淘宝首页
+//
+//  Created by SunSet on 14-8-14.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
